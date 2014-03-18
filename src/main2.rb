3455d51ff9d2fe1388a01b242fe480a263fb64f3
huangtao <ht.anglenx@gmail.com>
2014-03-18 16:24:40 +0800

    fix new key word

--- src/main2.rb
+++ src/main2.rb
@@ -96,12 +96,12 @@ class FLine
 HERE
 
     def initialize(objectid,layerid,layername,point)
-        @attr = {:objectid => objectid,
-                 :layerid  => layerid,
-                 :layername=> layername,
-                 :type     => 1,
-                 :num      => point.size,
-                 :point    => point.join('\n')}
+        @attr = {:objectid =>objectid,
+                 :layerid  =>layerid,
+                 :layername=>layername,
+                 :type     =>1,
+                 :num      =>point.size,
+                 :point    =>point.join('\n')}
     end
 
     def to_s
@@ -134,12 +134,13 @@ class FPolygon
 HERE
 
     def initialize(objectid,layerid,layername,line)
-        @attr = {:objectid => objectid,
-                 :layerid  => layerid,
-                 :layername=> layername,
-                 :point    => new Point(1,1),
-                 :num      => line.size,
-                 :line     => line.join(',')}
+        p = new Point(1,1)
+        @attr = {:objectid =>objectid,
+                 :layerid  =>layerid,
+                 :layername=>layername,
+                 :point    =>p,
+                 :num      =>line.size,
+                 :line     =>line.join(',')}
     end
 
     def to_s
@@ -219,7 +220,8 @@ class VctDataset
                           @prefix[:name] + @layercode,
                           type,
                           @prefix[:table] + @layercode,
-                          objectid)
+                          objectid,
+                          @field)
         @layers << layer
         return layer
     end
@@ -230,7 +232,7 @@ class VctDataset
 end
 
 class VctFile
-    def initialize(fileName, size) 
+    def initialize(fileName) 
         if File.exist? fileName
             puts "#{fileName} is exist. Now delete!" 
             File.delete fileName
@@ -308,36 +310,52 @@ class VctCreater
 
     def fake_head
         @vct.srs = <<HERE
-
+Datamark: LANDUSE.VCTFILE
+Version: 2.0
+Unit: M
+Dim: 2
+Topo: 1
+Coordinate: M
+Projection: Gauss-Krueger Projection
+Spheroid: IAG-75 Spheroid
+Parameters: 6378140,6356755
+Meridinan: 114
+MinX: 38394015.414
+MinY:  3103134.261
+MaxX: 38401706.631
+MaxY:  3120945.278
+Scale: 10000
+Date: 20100611
+Separator: ,
 HERE
     end
 
     def fake_point
-        (1..@pointNum).each do |p|
-            layer = @vct.create_layer("Point",p, @table) if p % 100 == 1
-            i = (p - 1) / @n
-            j = (p - 1) % @n
+        # (1..@pointNum).each do |p|
+        #     layer = @vct.create_layer("Point",p, @table) if p % 100 == 1
+        #     i = (p - 1) / @n
+        #     j = (p - 1) % @n
 
-            point = new FPoint(layer.id,layer.name,new Point(i,j))
-            attribute = new Attribute(layer.id,@attr)
-            feat = layer.create_feature(point,attribute)
-        end
+        #     point = new FPoint(layer.id,layer.name,new Point(i,j))
+        #     attribute = new Attribute(layer.id,@attr)
+        #     feat = layer.create_feature(point,attribute)
+        # end
     end
 
     def fake_line
         id = @pointNum
-        (1..@lineNum).each do |l|
-            id = id + l
-            layer = @vct.create_layer("Line",id,@table) if l % 100 == 1
-            start_point,end_point = calculate_line_point(l)
+        # (1..@lineNum).each do |l|
+        #     id = id + l
+        #     layer = @vct.create_layer("Line",id,@table) if l % 100 == 1
+        #     start_point,end_point = calculate_line_point(l)
 
-            pointNum = rand(1..10)
-            points = generateLinePoint(start_point,end_point,pointNum)
+        #     pointNum = rand(1..10)
+        #     points = generateLinePoint(start_point,end_point,pointNum)
 
-            line = new FLine(layer.id,layer.name,points)
-            attribute = new Attribute(layer.id,@attr)
-            feat = layer.create_feature(line,attribute)
-        end
+        #     line = new FLine(layer.id,layer.name,points)
+        #     attribute = new Attribute(layer.id,@attr)
+        #     feat = layer.create_feature(line,attribute)
+        # end
     end
 
     def calculate_line_point(l)
@@ -351,7 +369,7 @@ HERE
             j = (l-n)%(2*@n -1)
             pstart = Point.new(i,j)
             pend = Point.new(i+1,j)
-        end 
+        end
         return pstart,pend
     end
 
@@ -365,7 +383,7 @@ HERE
         n.times do |n|
             point = Point.new(s.x + width*(n+1),s.y + heigh*(n+1))
             points << point
-        end 
+        end
 
         points << e
 
@@ -373,34 +391,66 @@ HERE
     end
 
     def fake_polygon
-        id = @pointNum + @lineNum 
-        (1..@polygonNum).each do |k|
-            id = id +1
-            layer = @vct.create_layer("Polygon",id,@table) if k % 100 == 1
+        id = @pointNum + @lineNum
+       #  (1..@polygonNum).each do |k|
+       #      id = id +1
+       #      layer = @vct.create_layer("Polygon",id,@table) if k % 100 == 1
 
-            l1 = (k-1)/(@n-1)*(2*@n-1) + (k-1)%(@n-1) +1
-            l2 = l1+@n
-            l3 = l1+2*@n-1 
-            l4 = l1+@n-1 
+       #      l1 = (k-1)/(@n-1)*(2*@n-1) + (k-1)%(@n-1) +1
+       #      l2 = l1+@n
+       #      l3 = l1+2*@n-1 
+       #      l4 = l1+@n-1 
 
-            polygon = new FPolygon(layer.id,layer.name,
-                        ["#{l1+@pointNum}",
-                        "#{l2+@pointNum}",
-                        "-#{l3+@pointNum}",
-                        "-#{l4+@pointNum}"])
+       #      polygon = new FPolygon(layer.id,layer.name,
+       #                  ["#{l1+@pointNum}",
+       #                  "#{l2+@pointNum}",
+       #                  "-#{l3+@pointNum}",
+       #                  "-#{l4+@pointNum}"])
 
-            attribute = new Attribute(layer.id,@attr)
+       #      attribute = new Attribute(layer.id,@attr)
 
-            feat = layer.create_feature(polygon,attribute)
-       end
-    end
+       #      feat = layer.create_feature(polygon,attribute)
+       # end
+   end
 
-    def fake_table_structure
-        @table = [] 
+   def fake_table_structure
+
+        t = <<HERE
+BSM,Integer,10
+YSDM,Char,10
+KZDMC,Char,50
+KZDDH,Char,10
+KZDLX,Char,10
+KZDDJ,Char,30
+BSLX,Char,2
+BZLX,Char,2
+KZDZT,Char,100
+DZJ,Varbin
+X80,Float,10,3
+Y80,Float,10,3
+Z80,Float,10,3
+X54,Float,10,3
+Y54,Float,10,3
+Z54,Float,10,3
+HERE
+        fields = t.split('\n')
+
+        fields.each do |f|
+            field = f.split(',')
+            if field.size < 4
+                field << 0
+                field << 0
+            end
+            n,t,w,p = field
+            @table << FieldType.new(n,t,w,p)
+        end
     end
 
     def fake_attribute
-        @attribute = []
+        @attr = ["xxmc","iwea","zjfda",
+                "vlfda","et","md","rhw","fhc",
+                2.9,3.2,2.2,
+                4.5,3.2,2.1]
     end
 
     def fake
@@ -418,23 +468,23 @@ def dataset2file(vctds,vctfile)
 end
 
 def fake_vct(name,size)
-    vct_ds   = new VctDataset(name)
-    vct_fake = new VctCreater(vct_ds)
+    vct_ds   = VctDataset.new(name)
+    vct_fake = VctCreater.new(vct_ds,size)
     vct_fake.fake
     return vct_ds
 end
 
 def create_file(name)
-    return new VctFile(name)
+    return VctFile.new(name)
 end
 
 def main(argv)
     size = argv[0] || 2
     name = argv[1] || 'TEST.VCT'
     vct_ds   = fake_vct(name,size)
-    vct_file = create_file(name) 
+    vct_file = create_file(name)
     dataset2file(vct_ds,vct_file)
     vct_file.close
 end
 
-main(AEGV)
+main(ARGV)
