d01a122bd9b9c059d9e3d50a9abb378f51e24512
huangtao <ht.anglenx@gmail.com>
2014-04-21 16:18:27 +0800

    add lang

--- src/ruby/vctgeometry.rb
+++ src/ruby/vctgeometry.rb
@@ -31,15 +31,18 @@ module Vct
             end
 
             def size
-                @points.size
+                @num + 2
             end
 
             def to_s
-                @points.join("\n")
-            end
-
-            def populate()
-
+                points = []
+                width = (@end_point.x - @start_point.x).to_f/(@num + 1)
+                heigh =  (@end_point.y - @start_point.y).to_f/(@num + 1)
+                @num.times do |n|
+                    point = Point.new(@start_point.x + width*(n+1),@start_point.y + heigh*(n+1))
+                    points << point
+                end
+                points.join("\n")
             end
         end
 
@@ -65,5 +68,22 @@ module Vct
                 @lineid.join(",")
             end
         end
+
+        def generateLinePoint(s,e,n)
+            line = Line.new()
+            width = (e.x-s.x).to_f/(n+1)
+            heigh = (e.y-s.y).to_f/(n+1)
+
+            line.add(s)
+
+            n.times do |n|
+                point = Point.new(s.x + width*(n+1),s.y + heigh*(n+1))
+                line.add(point)
+            end
+
+            line.add(e)
+
+        return line
+    end
     end
 end
